a597002d49ea98e7ad1cadb8a02c33d31c045132
StuartFarmer <email@stuartfarmer.me>
2016-08-15 20:55:41 -0400

    more stuff, almost ready for putting online again

--- hyperopt-test.py
+++ hyperopt-test.py
@@ -47,18 +47,18 @@ phenotypeSpace = [
 			hp.quniform('max_stagnation', 0, 100, 1),
 
 			hp.quniform('elitism', 0, 100, 1),
-			hp.uniform('survival_threshold', 0, 1)
+			hp.uniform('survival_threshold', 0, 1),
 
-			hp.uniform('pos_x', -100, 100)
-			hp.uniform('pos_y', -100, 100)
-			hp.uniform('vel_x', -100, 100)
-			hp.uniform('vel_y', -100, 100)
-			hp.uniform('angle', -100, 100)
-			hp.uniform('angle_vel', -100, 100)
-			hp.uniform('leg_one', -100, 100)
-			hp.uniform('leg_two', -100, 100)
-			hp.uniform('total_frames', -100, 100)
-			hp.uniform('fuel_use', -100, 100)
+			hp.uniform('pos_x', -100, 100),
+			hp.uniform('pos_y', -100, 100),
+			hp.uniform('vel_x', -100, 100),
+			hp.uniform('vel_y', -100, 100),
+			hp.uniform('angle', -100, 100),
+			hp.uniform('angle_vel', -100, 100),
+			hp.uniform('leg_one', -100, 100),
+			hp.uniform('leg_two', -100, 100),
+			hp.uniform('total_frames', -100, 100),
+			hp.uniform('fuel_use', -100, 100),
 			hp.uniform('reward', -100, 100)
 			]
 
@@ -73,6 +73,7 @@ def eval_fitness(genomes):
 		observation, reward, done, info = env.step(env.action_space.sample())
 		frames = 0
 
+		fuel_use = 0
 		fitness = 0
 		
 		while 1:
@@ -81,16 +82,14 @@ def eval_fitness(genomes):
 			index, value = max(enumerate(output), key=operator.itemgetter(1))
 			action = index
 			
-			#print(observation)
-
 			try:
 				observation, reward, done, info = env.step(action)
 			except AssertionError:
 				fitness -= 1000000
 				done = True
-			fitness += reward
-			#fitness += action
-			#print(action)
+
+			if reward < 100 and reward > -100:
+				fuel_use -= reward
 
 			env.render()
 			frames += 1
@@ -99,22 +98,38 @@ def eval_fitness(genomes):
 				done = True
 
 			if done:
+				end_state = list(observation)
+				end_state.append(frames)
+				end_state.append(fuel_use)
+				end_state.append(reward)
+
+				# make x, y, an angle abs
+				end_state[0] = abs(end_state[0])
+				end_state[1] = abs(end_state[1])
+				end_state[2] = abs(end_state[1])
+				end_state[4] = abs(end_state[4])
+				
+				for x, val in enumerate(fitness_weights):
+				 	fitness += fitness_weights[x] * end_state[x]
+
+				#print(fitness)
+				#print(reward-fuel_use)
 				env.reset()
 				break
-
 		# evaluate the fitness
+		fitnesses.append(reward-fuel_use)
 		g.fitness = fitness
-		fitnesses.append(fitness)
 	average_fitnesses.append(np.mean(fitnesses))
 	print(average_fitnesses)
 
 def adjustPhenotypes(args):
 	fitness_weights[:] = []
 	average_fitnesses[:] = []
-	hidden_nodes, max_weight, min_weight, feedforward, weight_stdev, pop_size, prob_add_conn, prob_add_node, prob_delete_conn, prob_delete_node, prob_mutate_bias, bias_mutation_power, prob_mutate_response, response_mutation_power, prob_mutate_weight, prob_replace_weight, weight_mutation_power, prob_mutate_activation, prob_toggle_link, compatibility_threshold, excess_coefficient, disjoint_coefficient, weight_coefficient, max_stagnation, elitism, survival_threshold = args
+	hidden_nodes, max_weight, min_weight, feedforward, weight_stdev, pop_size, prob_add_conn, prob_add_node, prob_delete_conn, prob_delete_node, prob_mutate_bias, bias_mutation_power, prob_mutate_response, response_mutation_power, prob_mutate_weight, prob_replace_weight, weight_mutation_power, prob_mutate_activation, prob_toggle_link, compatibility_threshold, excess_coefficient, disjoint_coefficient, weight_coefficient, max_stagnation, elitism, survival_threshold, pos_x, pos_y, vel_x, vel_y, angle, angle_vel, leg_one, leg_two, total_frames, fuel_use, reward = args
 
 	pop = population.Population(config_path)
 	
+	# edit the configiration of the neural networks
 	pop.config.hidden_nodes = hidden_nodes
 	pop.config.max_weight = max_weight
 	pop.config.min_weight = min_weight
@@ -143,8 +158,21 @@ def adjustPhenotypes(args):
 	pop.config.elitism = elitism
 	pop.config.survival_threshold = survival_threshold
 
-	#pop = population.Population(config_path)
-	pop.run(eval_fitness, 10)
+	# add weights to list for fitness evaluations
+	fitness_weights.append(pos_x)
+	fitness_weights.append(pos_y)
+	fitness_weights.append(vel_x)
+	fitness_weights.append(vel_y)
+	fitness_weights.append(angle)
+	fitness_weights.append(angle_vel)
+	fitness_weights.append(leg_one)
+	fitness_weights.append(leg_two)
+	fitness_weights.append(total_frames)
+	fitness_weights.append(fuel_use)
+	fitness_weights.append(reward)
+
+	# run NEAT!
+	pop.run(eval_fitness, 25)
 
 	change_in_average_fitnesses = [j-i for i, j in zip(average_fitnesses[:-1], average_fitnesses[1:])]
 	print(change_in_average_fitnesses)
